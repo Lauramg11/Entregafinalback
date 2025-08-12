bb519120646b4fd86a4c93d8ef9f154014620247
Alejandra Giraldo <alejagr.ag@gmail.com>
2025-08-12 17:05:54 -0500

    se crean mas cambios

--- RegistroEIngreso/src/main/java/com/example/RegistroEIngreso/modelos/Usuario.java
+++ RegistroEIngreso/src/main/java/com/example/RegistroEIngreso/modelos/Usuario.java
@@ -1,12 +1,16 @@
 package com.example.RegistroEIngreso.modelos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 @Entity
 public class Usuario {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Integer id;
     private String nombre;
     private String contraseña;
